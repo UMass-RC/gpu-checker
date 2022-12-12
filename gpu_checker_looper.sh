49a420b096e1e31c0b85960e24204473ac789bc5
simon <simonleary@umass.edu>
2022-12-11 21:55:23 -0500

    absolute paths in looper script

--- gpu_checker_looper.sh
+++ gpu_checker_looper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    ./gpu_checker_venv/bin/python ./gpu_checker.py &> ../logs/gpu_checker_looper.log
+    /opt/gpu_checker/gpu_checker_venv/bin/python /opt/gpu_checker/gpu_checker.py &> /opt/logs/gpu_checker_looper.log
     sleep 5
 done
